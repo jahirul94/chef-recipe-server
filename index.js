60d0ffcfb3994ab946ad100b95ed3b845f8f53ef
jahirul94 <jahirulislam41185@gmail.com>
2023-05-02 03:56:29 +0600

    details data added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5000 ;
 const data = require ('./data.json')
+const cors = require('cors');
+
+app.use(cors())
 app.get('/', ( req , res )=>{
     res.send('shape is cooking')
 
