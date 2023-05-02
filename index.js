631ecb6d7303492b8fd5c1749598508083fc457c
jahirul94 <jahirulislam41185@gmail.com>
2023-05-02 21:47:45 +0600

    update data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000 ;
 const data = require ('./data.json')
+const details = require('./recipes_details.json')
 const cors = require('cors');
 
 app.use(cors())
@@ -12,6 +13,12 @@ app.get('/', ( req , res )=>{
 
 app.get('/data' , (req , res ) =>{
      res.send(data)
+})
+
+app.get('/:id' , (req , res ) =>{
+     const paramsId = req.params.id;
+     const selectedItems = details.filter(dt => dt.category_id == paramsId )
+     res.send(selectedItems)
 })
 
 app.listen(port , ()=>{
